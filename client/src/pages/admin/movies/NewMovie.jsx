6934a53d528a7ff0e497ca124eb62b2ef03a59b1
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-08-06 11:09:20 +0300

    up

--- client/src/pages/admin/movies/NewMovie.jsx
+++ client/src/pages/admin/movies/NewMovie.jsx
@@ -1,9 +1,17 @@
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { AdminMovieForm } from "../../../components/forms/AdminMovieForm";
 
 export function AdminNewMoviePage() {
     return (
-        <main>
-            <AdminPageTitle title="New movie" />
+          <main>
+            <AdminPageTitle title="New category" />
+
+            <div className="container">
+                <div className="row">
+                    <AdminMovieForm />
+                </div>
+            </div>
         </main>
     );
 }
+
