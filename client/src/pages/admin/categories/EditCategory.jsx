46be955583a97d99c2b8a909b060f11e5d2654b6
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-08-06 09:18:39 +0300

    update

--- client/src/pages/admin/categories/EditCategory.jsx
+++ client/src/pages/admin/categories/EditCategory.jsx
@@ -2,13 +2,24 @@ import { AdminPageTitle } from "../../../components/AdminPageTitle";
 import { AdminCategoryForm } from "../../../components/forms/AdminCategoryForm";
 
 export function AdminEditCategoryPage() {
+
+    const category = {
+        title: 'Action',
+        url: 'action',
+        description: 'Very action muvie',
+        status:'published',
+    }
     return (
         <main>
             <AdminPageTitle title="Edit category" />
 
             <div className="container">
                 <div className="row">
-                    <AdminCategoryForm />
+                    <AdminCategoryForm
+                      category={category}
+                    
+                    
+                    />
                 </div>
             </div>
         </main>
